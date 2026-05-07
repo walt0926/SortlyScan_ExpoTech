236ac52b478ad5116a38c4d2658d2e8e818d5aef
tatianadeveloper135 <fatima.mendez2026@merlet.superate.org.sv>
2026-05-07 09:14:40 -0600

    css del inicio de sesion

--- public/Iniciodesesion.php
+++ public/Iniciodesesion.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
   <meta name="color-scheme" content="light dark">
-  <link rel="stylesheet" href="public/CSS/styleIniciodeSesion.css">
-  <link rel="stylesheet" href="public/JS/scriptIniciodesesion.js">
+  <link rel="stylesheet" href="CSS/styleIniciodeSesion.css">
+  <link rel="stylesheet" href="JS/scriptIniciodesesion.js">
 </head>
 <body>
   <div class="tailwind">
